2bb19a3ae14cb88602f976e6b902f1413d2741fb
itodobien <itodobien@gmail.com>
2025-04-07 13:09:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string name;
+int age;
+string address;
+
+Console.WriteLine("""
+                  Enter your name:
+                  """);
+
+name = Console.ReadLine();
+Console.WriteLine("""
+                  Enter your age:
+                  """);
+age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("""
+                  Enter your address:
+                  """);
+address = Console.ReadLine();
+
+
+
+Console.WriteLine("""
+                  Hello, {name}
+                  Your age is {age}
+                  Your address is {address}
+                  """);
